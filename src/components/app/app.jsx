5b13ba999b4ace8adc7135e3a65cad707a771e67
hoodsey20 <hoodsey20@gmail.com>
2019-05-04 14:18:13 +0700

    добавил и реализовал proptypes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import Main from '../main/main.jsx';
 
+const movies = [`Fantastic Beasts`, `Bohemian Rhapsody`, `Macbeth`];
+
 export default class App extends React.Component {
   render() {
-    return <Main />;
+    return (
+      <Main
+        movies={movies}
+      />
+    );
   }
 }
