d4c0c9033f2a8a1bca9b43bb3e3299606f030cdc
hoodsey20 <hoodsey20@gmail.com>
2019-05-26 22:57:06 +0700

    добавляет поддержку redux-dev-tools

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import {logger} from 'redux-logger';
 
 import App from './components/app/app.jsx';
 import {reducer} from './reducer';
 
+const middlewares = [logger];
+
 function init() {
-  const store = createStore(
-      reducer,
-      applyMiddleware(logger)
-  );
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  const store = createStore(reducer, composeEnhancers(
+      applyMiddleware(...middlewares)
+  ));
 
   ReactDOM.render(
       <Provider store={store}>
