c29c07010384bf26ddc724616f796876d798dc06
hoodsey20 <hoodsey20@gmail.com>
2019-05-26 23:25:14 +0700

    добавляет hoc для контроля активного плеера

---
+++ src/hocs/with-active-player/with-active-player.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+let timeoutID;
+const PLAY_DELAY = 1000;
+
+const withActivePlayer = (Component) => {
+  class WithActivePlayer extends React.PureComponent {
+    constructor(props) {
+      super(props);
+      this.state = {
+        activeCard: null,
+      };
+
+      this._cardHoverHandler = this._cardHoverHandler.bind(this);
+      this._cardLeaveHandler = this._cardLeaveHandler.bind(this);
+    }
+
+    _cardHoverHandler(movieId) {
+      timeoutID = setTimeout(() => {
+        this.setState({activeCard: movieId});
+      }, PLAY_DELAY);
+    }
+
+    _cardLeaveHandler() {
+      clearTimeout(timeoutID);
+      this.setState({activeCard: null});
+    }
+
+    render() {
+      const {activeCard} = this.state;
+      return (
+        <Component
+          {...this.props}
+          activeCard={activeCard}
+          cardHoverHandler={this._cardHoverHandler}
+          cardLeaveHandler={this._cardLeaveHandler}
+        />
+      );
+    }
+  }
+
+  return WithActivePlayer;
+};
+
+export default withActivePlayer;
