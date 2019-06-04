d73b3638915aed9a3e6847349999c1d87ba526e7
hoodsey20 <hoodsey20@gmail.com>
2019-06-04 23:12:16 +0700

    добавляет компонент sign in

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
+import {connect} from 'react-redux';
+
+import NameSpace from '../../reducer/name-spaces';
 import Main from '../main/main.jsx';
+import SignIn from '../sign-in/sign-in.jsx';
 
-export const App = () => {
+const App = ({isAuthorizationRequired}) => {
   return (
-    <Main />
+    <React.Fragment>
+      {isAuthorizationRequired ? (
+        <SignIn />
+      ) : (
+        <Main />
+      )}
+    </React.Fragment>
   );
 };
+
+const mapStateToProps = (state) => ({
+  isAuthorizationRequired: state[NameSpace.USER].isAuthorizationRequired,
+});
+
+App.propTypes = {
+  isAuthorizationRequired: PropTypes.bool.isRequired,
+};
+
+export default connect(mapStateToProps)(App);
