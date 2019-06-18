2702721975e7f9e4ced9ee93d439fab408466970
hoodsey20 <hoodsey20@gmail.com>
2019-06-18 21:56:57 +0700

    добавляет вёрстку для страницы авторизации

--- src/components/sign-in/sign-in.jsx
+++ src/components/sign-in/sign-in.jsx
@@ -14,27 +14,40 @@ export const SignIn = ({
   error,
 }) => {
   return (
-    <div className="sign-in user-page__content">
-      <form action="#" className="sign-in__form" onSubmit={(evt) => {
-        makeAuth(
-            formSubmitHandler(evt)
-        );
-      }}>
-        <div className="sign-in__fields">
-          <div className="sign-in__field">
-            <input onChange={inputHandler} className="sign-in__input" type="email" placeholder="Email address" name="email" id="user-email" />
-            <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
-          </div>
-          <div className="sign-in__field">
-            <input onChange={inputHandler} className="sign-in__input" type="password" placeholder="Password" name="password" id="user-password" />
-            <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
-          </div>
+    <div className="user-page">
+      <header className="page-header user-page__head">
+        <div className="logo">
+          <a href="main.html" className="logo__link">
+            <span className="logo__letter logo__letter--1">W</span>
+            <span className="logo__letter logo__letter--2">T</span>
+            <span className="logo__letter logo__letter--3">W</span>
+          </a>
         </div>
-        <div className="sign-in__submit">
-          <button className="sign-in__btn" type="submit">Sign in</button>
-        </div>
-      </form>
-      {error && <h3>{error}</h3>}
+
+        <h1 className="page-title user-page__title">Sign in</h1>
+      </header>
+      <div className="sign-in user-page__content">
+        <form action="#" className="sign-in__form" onSubmit={(evt) => {
+          makeAuth(
+              formSubmitHandler(evt)
+          );
+        }}>
+          <div className="sign-in__fields">
+            <div className="sign-in__field">
+              <input onChange={inputHandler} className="sign-in__input" type="email" placeholder="Email address" name="email" id="user-email" />
+              <label className="sign-in__label visually-hidden" htmlFor="user-email">Email address</label>
+            </div>
+            <div className="sign-in__field">
+              <input onChange={inputHandler} className="sign-in__input" type="password" placeholder="Password" name="password" id="user-password" />
+              <label className="sign-in__label visually-hidden" htmlFor="user-password">Password</label>
+            </div>
+          </div>
+          <div className="sign-in__submit">
+            <button className="sign-in__btn" type="submit">Sign in</button>
+          </div>
+        </form>
+        {error && <div className="sign-in__message"><p>{error}</p></div>}
+      </div>
     </div>
   );
 };
