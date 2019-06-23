7cb978964c40a704a92f09ff44a28a750d521aa2
hoodsey20 <hoodsey20@gmail.com>
2019-06-23 15:03:43 +0700

    реализует функционал добавления в избранное для промо-фильма

--- src/components/main/main.jsx
+++ src/components/main/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 
+import {Operation} from '../../reducer/movies/actions';
+
 import Logo from '../logo/logo.jsx';
 import UserBlock from '../user-block/user-block.jsx';
 import MoviesList from '../movies-list/movies-list.jsx';
@@ -10,18 +12,23 @@ import {userDataSelector} from '../../reducer/user/selectors';
 import {promoSelector, filteredMoviesSelector} from '../../reducer/movies/selectors';
 import withPlayerController from '../../hocs/with-player-controller/with-player-controller';
 
-const Main = ({user, history, playerVisibleHandler, movie, movies}) => {
-  if (!movie) {
-    return null;
-  }
+const Main = ({
+  user,
+  history,
+  playerVisibleHandler,
+  movie,
+  movies,
+  setFavoriteStatus,
+}) => {
   const {
+    id,
     name,
     released,
     genre,
     backgroundImage,
     posterImage,
-  } = movie;
-
+    isFavorite,
+  } = movie || {};
   return (
     <React.Fragment>
       <div className="visually-hidden">
@@ -31,26 +38,14 @@ const Main = ({user, history, playerVisibleHandler, movie, movies}) => {
           <g id="Page-1" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
             <polygon id="+" fill="#EEE5B5" points="10.777832 11.2880859 10.777832 19.5527344 8.41650391 19.5527344 8.41650391 11.2880859 0.627929688 11.2880859 0.627929688 8.92675781 8.41650391 8.92675781 8.41650391 0.662109375 10.777832 0.662109375 10.777832 8.92675781 18.5664062 8.92675781 18.5664062 11.2880859"/>
           </g>
-        </symbol><symbol id="full-screen" viewBox="0 0 27 27">
-          <path fillRule="evenodd" clipRule="evenodd" d="M23.8571 0H16V3.14286H23.8571V11H27V3.14286V0H23.8571Z" fill="#FFF9D9" fillOpacity="0.7"/>
-          <path fillRule="evenodd" clipRule="evenodd" d="M27 23.8571V16H23.8571V23.8571H16V27H23.8571H27L27 23.8571Z" fill="#FFF9D9" fillOpacity="0.7"/>
-          <path fillRule="evenodd" clipRule="evenodd" d="M0 3.14286L0 11H3.14286L3.14286 3.14286L11 3.14286V0H3.14286H0L0 3.14286Z" fill="#FFF9D9" fillOpacity="0.7"/>
-          <path fillRule="evenodd" clipRule="evenodd" d="M3.14286 27H11V23.8571H3.14286L3.14286 16H0L0 23.8571V27H3.14286Z" fill="#FFF9D9" fillOpacity="0.7"/>
         </symbol><symbol id="in-list" viewBox="0 0 18 14">
           <path fillRule="evenodd" clipRule="evenodd" d="M2.40513 5.35353L6.1818 8.90902L15.5807 0L18 2.80485L6.18935 14L0 8.17346L2.40513 5.35353Z" fill="#EEE5B5"/>
-        </symbol><symbol id="pause" viewBox="0 0 14 21">
-          <title>Artboard</title>
-          <desc>Created with Sketch.</desc>
-          <g id="Artboard" stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-            <polygon id="Line" fill="#EEE5B5" fillRule="nonzero" points="0 -1.11910481e-13 4 -1.11910481e-13 4 21 0 21"/>
-            <polygon id="Line" fill="#EEE5B5" fillRule="nonzero" points="10 -1.11910481e-13 14 -1.11910481e-13 14 21 10 21"/>
-          </g>
         </symbol><symbol id="play-s" viewBox="0 0 19 19">
           <path fillRule="evenodd" clipRule="evenodd" d="M0 0L19 9.5L0 19V0Z" fill="#EEE5B5"/>
         </symbol></svg>
       </div>
 
-      <section className="movie-card">
+      {movie && <section className="movie-card">
         <div className="movie-card__bg">
           <img src={backgroundImage} alt={name} />
         </div>
@@ -82,19 +77,31 @@ const Main = ({user, history, playerVisibleHandler, movie, movies}) => {
                   </svg>
                   <span>Play</span>
                 </button>
-                <button className="btn btn--list movie-card__button" type="button">
-                  <svg viewBox="0 0 19 20" width="19" height="20">
-                    <use xlinkHref="#add"></use>
-                  </svg>
-                  <span>My list</span>
-                </button>
+                {user &&
+                  <button
+                    className="btn btn--list movie-card__button"
+                    type="button"
+                    onClick={() => setFavoriteStatus(id, isFavorite)}
+                  >
+                    {isFavorite ? (
+                      <svg viewBox="0 0 18 14" width="18" height="14">
+                        <use xlinkHref="#in-list"></use>
+                      </svg>
+                    ) : (
+                      <svg viewBox="0 0 19 20" width="19" height="20">
+                        <use xlinkHref="#add" />
+                      </svg>
+                    )}
+                    <span>My list</span>
+                  </button>
+                }
               </div>
             </div>
           </div>
         </div>
-      </section>
+      </section>}
 
-      <div className="page-content">
+      {movies && <div className="page-content">
         <section className="catalog">
           <h2 className="catalog__title visually-hidden">Catalog</h2>
           <GenresList />
@@ -108,7 +115,7 @@ const Main = ({user, history, playerVisibleHandler, movie, movies}) => {
             <p>© 2019 What to watch Ltd.</p>
           </div>
         </footer>
-      </div>
+      </div>}
     </React.Fragment>
   );
 };
@@ -118,6 +125,16 @@ const mapStateToProps = (state) => ({
   movie: promoSelector(state),
   movies: filteredMoviesSelector(state),
 });
+
+const mapDispatchToProps = (dispatch) => ({
+  setFavoriteStatus: (id, status) => {
+    dispatch(Operation.sendNewFavoriteStatus(id, status));
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(
+    withPlayerController(Main)
+);
 
 Main.propTypes = {
   movies: PropTypes.array,
@@ -131,6 +148,7 @@ Main.propTypes = {
     push: PropTypes.func
   }),
   playerVisibleHandler: PropTypes.func,
+  mapDispatchToProps: PropTypes.func,
   movie: PropTypes.shape({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
@@ -138,9 +156,6 @@ Main.propTypes = {
     genre: PropTypes.string,
     backgroundImage: PropTypes.string,
     posterImage: PropTypes.string,
+    isFavorite: PropTypes.bool,
   }),
 };
-
-export default connect(mapStateToProps)(
-    withPlayerController(Main)
-);
